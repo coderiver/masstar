47005b3ddf306333fa0c9564636a6deb84fa29aa
buschidos <sergey.snjozyk@gmail.com>
2015-07-07 17:41:28 +0300

    upd

--- site/js/main.js
+++ site/js/main.js
@@ -12728,10 +12728,11 @@ var Slider = require('./modules/slider.js');
 
 $(document).ready(function() {
 
-    var menu         = $('.menu:not(.menu_opened)');
-    var slider       = $('.slider:not(.slider_narrow)');
-    var sliderNarrow = $('.slider.slider_narrow');
-    var menuOpened   = $('.menu.menu_opened');
+    var menu          = $('.menu:not(.menu_opened)');
+    var slider        = $('.slider:not(.slider_narrow)');
+    var sliderNarrow  = $('.slider.slider_narrow');
+    var menuOpened    = $('.menu.menu_opened');
+    var containerMore = $('.container-more');
 
     if (menu.length) {
         menu = new Menu(menu);
@@ -12747,13 +12748,28 @@ $(document).ready(function() {
         slider.each(function(index, el) {
             new Slider(el);
         });
-    };
+    }
 
     if (sliderNarrow.length) {
         sliderNarrow.each(function(index, el) {
             new Slider(el);
         });
-    };
+    }
+
+    if (containerMore.length) {
+        containerMore.each(function(index, el) {
+            var button  = $(this).find('.container-more__btn');
+            var content = $(this).find('.container-more__content');
+            button.on('click', function(e) {
+                var text    = button.text();
+                var altText = button.attr('data-alt-text');
+                e.preventDefault();
+                content.slideToggle(400);
+                button.text(altText);
+                button.attr('data-alt-text', text);
+            });
+        });
+    }
 
 });
 
